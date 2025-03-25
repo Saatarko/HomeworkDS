53adb58341cb00421ad76f152cfd1aa209efb380
Saatarko <saatarko@tut.by>
2025-03-25 18:21:16 +0300

    Develop (#33)
    * fix some bugs
    
    * add homework_car homework_mobile, models folder
    
    * add predicted csv
    
    * add homework_dt_rf_xgb
    
    * add homework_params
    
    * add .env
    
    * add_regression
    
    * add tensor folder
    
    * change app
    
    * fix

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
+
+import subprocess
+
 from flask import Flask
 
 app = Flask(__name__)
 
-# Запускаем TensorBoard
-os.system("tensorboard --logdir=tensorboard --host 0.0.0.0 --port 10000 &")
+
+# Получаем порт из переменной окружения
+PORT = int(os.getenv("PORT", 8080))
+
+# Запускаем TensorBoard на порту 10000
+subprocess.Popen(f"tensorboard --logdir=tensorboard --port=10000 --host=0.0.0.0", shell=True)
 
 @app.route('/')
-def home():
-    return "TensorBoard работает! Перейдите по ссылке: <a href='/tensorboard'>TensorBoard</a>"
+def index():
+    return "Flask App is Running!"
 
-@app.route('/tensorboard')
-def tensorboard():
-    return "Перейдите по ссылке: <a href='http://YOUR_RENDER_URL:10000'>TensorBoard</a>"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=PORT)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
